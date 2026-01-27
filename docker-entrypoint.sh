7e537e9bf3d9e19fd20b5b7459695618ad70a386
Nikolay Stoykow <gitlab@stoykow.de>
2026-01-27 12:59:01 +0100

    ok

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,10 @@ APP_DIR="/var/www/html"
 
 if [ -f "$APP_DIR/composer.json" ]; then
   if [ ! -f "$APP_DIR/vendor/autoload.php" ]; then
-    COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --no-interaction
+    # Try install first; if lock is out of date, fall back to update.
+    if ! COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --no-interaction; then
+      COMPOSER_ALLOW_SUPERUSER=1 composer update --no-dev --no-interaction
+    fi
   fi
 else
   if [ ! -f "$APP_DIR/vendor/autoload.php" ]; then
